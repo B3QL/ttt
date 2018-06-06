9be60ab7824ef554b77ae6ca0c886918941b0491
Bartłomiej Kurzeja <bartlomiej+gitlab@kurzeja.it>
2018-06-06 17:19:20 +0200

    Implement classic Qlearning player

--- players.py
+++ players.py
@@ -1,4 +1,5 @@
-from random import choice
+from collections import defaultdict
+from random import choice, random
 
 
 class RandomPlayer:
@@ -17,3 +18,53 @@ class HumanPlayer:
             except ValueError:
                 pass
         return put_symbol(cell)
+
+
+class QLearningPlayer:
+    def __init__(self, learning_rate=0.1, discount_factor=0.1, exploration_factor=0.1):
+        self._alpha = learning_rate
+        self._gamma = discount_factor
+        self._eps = exploration_factor
+        self._q_table = defaultdict(dict)
+        self._last = None
+
+    def make_move(self, game, put_symbol):
+        state = game.state
+        best_action = self._pi(state)
+        if random() < self._eps or best_action is None:
+            best_action = choice(state.empty_cells)
+            self._q_table[state][best_action] = 0
+
+        result = put_symbol(best_action)
+        if result:
+            next_state = game.state
+            self._last = (state, next_state, best_action)
+            self._learn(*self._last)
+        return result
+
+    def reward(self, value):
+        self._learn(*self._last, reward=value)
+        self._last = None
+
+    def _learn(self, state, next_state, action, reward=0):
+        try:
+            rate_action = self._Q(next_state, self._pi(next_state))
+        except KeyError:
+            rate_action = 0
+
+        learned_value = reward + self._gamma * rate_action
+        self._q_table[state][action] = (1 - self._alpha) * self._Q(state, action) + self._alpha * learned_value
+
+    def _Q(self, state, action):
+        return self._q_table.get(state, {})[action]
+
+    def _pi(self, state):
+        best = None
+        actions = self._q_table.get(state, {}).keys()
+        if actions:
+            score, best = max((self._Q(state, action), action) for action in actions)
+        return best
+
+    @property
+    def q_table(self):
+        return self._q_table
